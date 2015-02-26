b7078cc78e517b6f6c21ecd4e7e6a05b8589a61f
Zach Garwood <zachgarwood@gmail.com>
2015-02-25 18:22:01 -0600

    Updated User constructor
    ...to pull uuid from data store or otherwise create one

--- lib/user.dart
+++ lib/user.dart
@@ -2,12 +2,21 @@ library user;
 
 import 'package:angular/angular.dart';
 import 'package:di/annotations.dart';
+import 'package:lawndart/lawndart.dart';
+import 'package:uuid/uuid.dart';
 
 @Injectable()
 class User {
     String id;
 
-    User(this.id);
+    User() {
+        var store = new Store('SwipeFashion', 'Users');
+        if (id == null) {
+            var uuid = new Uuid();
+            store.open().then((_) => store.save(uuid.v5(Uuid.NAMESPACE_URL, Uri.base.host), 'userId'));
+        }
+        store.open().then((_) => id = store.getByKey('userId'));
+    }
 
     Map toJson() {
         Map json = new Map();
