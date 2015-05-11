4cc2f17ef590e867bfc45a58135a139103161058
zachgarwood <zachgarwood@gmail.com>
2015-05-10 23:21:02 -0500

    Created basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var modules_dir = __dirname + '/node_modules';
+
+var config = {
+  entry: {
+    library: [__dirname + '/library/main.jsx'],
+    vendor: []
+  },
+  output: {
+    path: __dirname + '/build',
+    filename: 'app.js'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
+  ],
+  module: {
+    loaders: [ 
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.jsx$/, loader: 'jsx-loader'}
+    ],
+    noParse: []
+  },
+  resolve: { alias: {} },
+  addVendor: function (name, path) {
+    this.resolve.alias[name] = path;
+    this.module.noParse.push(new RegExp('^' + path + '$'));
+    this.entry.vendor.push(name);
+  }
+}
+config.addVendor('react', modules_dir + '/react/dist/react.min.js');
+
+module.exports = config;
